bea76d08124ab2ba330f5355d1fc7990e262851f
AbhinavBajpai <3899186+AbhinavBajpai@users.noreply.github.com>
2026-09-06 02:01:14 +0100

    Replace Offside with Share Tech Mono on tickets

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "@fontsource/dm-sans/latin-700.css";
 import "@fontsource/manrope/latin-500.css";
 import "@fontsource/manrope/latin-600.css";
 import "@fontsource/manrope/latin-800.css";
-import "@fontsource/offside/latin-400.css";
+import "@fontsource/share-tech-mono/latin-400.css";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
